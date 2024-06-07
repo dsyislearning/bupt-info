f1bdd04b9660d1f4eabbc29a41d60c2a14cb0d9e
dsy <deng.siyang@outlook.com>
2024-06-07 20:18:01 +0800

    (frontend) added evaluation function

--- frontend/antd-demo/src/App.tsx
+++ frontend/antd-demo/src/App.tsx
@@ -4,14 +4,24 @@ import "./App.css";
 
 import axios from "axios";
 
-import { Input, Space, List, Typography } from "antd";
+import { Input, Space, List, Button, Card, Rate, Tag, Typography } from "antd";
 import type { SearchProps } from "antd/es/input";
 
 const { Search } = Input;
 
+interface Passage {
+  title: string;
+  summary: string;
+  context: string[];
+  score: number;
+}
+
 function App() {
-  const [list, setList] = useState<string[]>([]);
+  const [list, setList] = useState<Passage[]>([]);
   const [num_passages, setNumPassages] = useState<number>(0);
+  const [showContext, setShowContext] = useState<{ [index: number]: boolean }>(
+    {}
+  );
   const [hasSearched, setHasSearched] = useState<boolean>(false);
 
   const onSearch: SearchProps["onSearch"] = async (value, _e, info) => {
@@ -45,16 +55,70 @@ function App() {
           />
           {hasSearched && (
             <List
-              style={{ backgroundColor: "white" }}
+              style={{ backgroundColor: "white", alignItems: "flex-start"}}
               pagination={{ position: "bottom", align: "center" }}
               header={<div>{num_passages} passages found</div>}
               // footer={<div>Footer</div>}
               bordered
               dataSource={list}
               renderItem={(item, index) => (
-                <List.Item>
-                  <Typography.Text mark>[ITEM {index}]</Typography.Text> {item}
-                </List.Item>
+                <Card
+                  style={{textAlign: "left", width: "100%"}}
+                  title={
+                    <Typography.Title level={4} style={{ textAlign: "left" }}>
+                      {item.title}
+                    </Typography.Title>
+                  }
+                  extra={
+                    <Space>
+                      <Button
+                        type="primary"
+                        onClick={() =>
+                          setShowContext((pre) => ({
+                            ...pre,
+                            [index]: !pre[index],
+                          }))
+                        }
+                      >
+                        {showContext[index] ? "Hide Context" : "Show Context"}
+                      </Button>
+                    </Space>
+                  }
+                >
+                  <Space
+                    direction="vertical"
+                    style={{ alignItems: "flex-start" }}
+                  >
+                    <Space style={{alignItems: "flex-start"}}>
+                      <Tag
+                        style={{ textAlign: "center", alignContent: "left" }}
+                      >
+                        Score: {item.score}
+                      </Tag>
+                      <Rate allowClear={true} onChange={(rate: number) => {
+                        console.log("rate", rate, "for doc", index);
+
+                        axios.post(`http://localhost:5000/rate`, {
+                          doc_id: index,
+                          rating: rate
+                        })
+                      }}/>
+                    </Space>
+
+                    {!showContext[index] && (
+                      <Typography.Paragraph style={{ textAlign: "left" }}>
+                        {item.summary}
+                      </Typography.Paragraph>
+                    )}
+
+                    {showContext[index] &&
+                      item.context.map((paragraph, paragraph_id) => (
+                        <Typography.Paragraph style={{ textAlign: "left" }}>
+                          {paragraph}
+                        </Typography.Paragraph>
+                      ))}
+                  </Space>
+                </Card>
               )}
             />
           )}
